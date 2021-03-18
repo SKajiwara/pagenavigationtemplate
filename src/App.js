7868cce87c98d1220c13763b18f022f8550ff9ee
SKajiwara <gold.shin789@gmail.com>
2021-03-18 10:02:29 -0600

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,61 @@
 import React from "react";
 import "./style.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-export default function App() {
-  return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Login</Link>
+              </li>
+              <li>
+                <Link to="/courses">Courses</Link>
+              </li>
+              <li>
+                <Link to="/course">Course</Link>
+              </li>
+            </ul>
+          </nav>
+
+          {/* A <Switch> looks through its children <Route>s and
+              renders the first one that matches the current URL. */}
+          <Switch>
+            <Route path="/courses">
+              <Courses />
+            </Route>
+            <Route path="/course">
+              <Course />
+            </Route>
+            <Route path="/">
+              <Login />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
 }
+
+class Login extends React.Component {
+  render() {
+    return <h2>Login</h2>;
+  }
+}
+
+class Courses extends React.Component {
+  render() {
+    return <h2>Courses</h2>;
+  }
+}
+
+class Course extends React.Component {
+  render() {
+    return <h2>Course</h2>;
+  }
+}
+
+export default App;
