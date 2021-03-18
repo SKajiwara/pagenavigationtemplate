3eab57731606d974147ab66429fddfe8c38ea3f9
SKajiwara <gold.shin789@gmail.com>
2021-03-18 12:42:54 -0600

    3 files modified

--- src/App.js
+++ src/App.js
@@ -42,7 +42,37 @@ class App extends React.Component {
 
 class Login extends React.Component {
   render() {
-    return <h2>Login</h2>;
+    return (
+      <div>
+        <div className="main">
+          <p className="sign" align="center">
+            Sign in
+          </p>
+          <form className="form1">
+            <input
+              className="un "
+              type="text"
+              align="center"
+              placeholder="Username"
+            />
+            <input
+              className="pass"
+              type="password"
+              align="center"
+              placeholder="Password"
+            />
+            <a className="submit" align="center">
+              Sign in
+            </a>
+            <p className="forgot" align="center">
+              <a href="#">Forgot Password?</a>
+            </p>
+            <a href="#" />
+          </form>
+        </div>
+        <a href="#" />
+      </div>
+    );
   }
 }
 
