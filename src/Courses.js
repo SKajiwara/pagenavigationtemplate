409d6413f308d11587fb3b0fb3c2e28ea6461435
SKajiwara <gold.shin789@gmail.com>
2021-03-22 12:55:08 -0600

    1 file modified

--- src/Courses.js
+++ src/Courses.js
@@ -9,10 +9,32 @@ import {
 } from "react-router-dom";
 
 function Courses() {
-  let { categoryId } = useParams;
+  let { categoryId } = useParams();
+
+  // Temp Data
+  const categories = [
+    {
+      id: "DevCourses",
+      courses: [
+        { id: "234512425", name: "Pointman", url: "dsadsad" },
+        { id: "412515124", name: "Cloud", url: "wgasdada" },
+        { id: "741321317", name: "Mobile", url: "kvjgiuiu" }
+      ]
+    },
+    { id: "ProdCourses" },
+    { id: "SalesCourses" }
+  ];
+
+  // Find Course
+  const category = categories.find(({ id }) => id === categoryId);
+
+  // Handle Error: course category not found
+  if (!category) throw new Error("Course category not found (404)");
+
+  console.log(category);
   return (
     <div className="Courses">
-      <h3>{categoryId}</h3>
+      <h3>Course: {categoryId}</h3>
     </div>
   );
 }
